55d956558068c482521c31c8d30b9ab1d50d105d
GoRnNiK <109069800+GoRnNiK@users.noreply.github.com>
2022-07-11 12:33:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Lessons1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+            string surname;
+            string color;
+            Console.WriteLine("Enter your age");
+            age = Console.ReadLine();
+            Console.WriteLine("Enter your first and last name?");
+            surname = Console.ReadLine();
+            Console.WriteLine("Enter your favorite color");
+            color = Console.ReadLine();
+            Console.WriteLine("You are " + age + ", it's time to change something in life! "
+                + "You have a very beautiful name " + surname + ", but that's no reason to hire you."
+                + " But if your favorite color " + color + ", we'll think about it. " +
+                "Thank you, we will call you back. Do not see you again.");
+   
+        }
+    }
+}
